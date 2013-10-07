bf5d5938c21be097de4e57aad67041082e1b7bc5
Krause <stefan.krausekjaer@gmail.com>
2013-10-07 14:44:56 +0200

    chore(project4_5): added empty project files.

---
+++ Opg4AcceptorConnector/Acceptor.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "..\winsock2_wrapper\iEventHandler.h"
+
+class Acceptor :
+	public iEventHandler
+{
+public:
+
+	Acceptor(){
+
+	}
+
+	~Acceptor(){
+	}
+
+	void handleEvent(std::string data){
+
+	}
+
+};
+
