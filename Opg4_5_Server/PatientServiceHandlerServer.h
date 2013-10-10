77230e94f0c59f17f85cf026a71f7335f8b469d3
Krause <stefan.krausekjaer@gmail.com>
2013-10-10 20:57:36 +0200

    development(Server): Server has been finished.

--- Opg4_5_Server/PatientServiceHandlerServer.h
+++ Opg4_5_Server/PatientServiceHandlerServer.h
@@ -11,13 +11,36 @@ public:
 	}
 
 	PatientServiceHandlerServer(std::shared_ptr<Reactor> _dispatcher) : ServiceHandler(_dispatcher)	{
+		patientDatabase = make_shared <map<string, string>>();
 
+		pair<string, string> patient1("123456-1111", "Patient 1 info");
+		pair<string, string> patient2("123456-1222", "Patient 2 info");
+		pair<string, string> patient3("123456-1333", "Patient 3 info");
+		pair<string, string> patient4("123456-1444", "Patient 4 info");
+		pair<string, string> patient5("123456-1555", "Patient 5 info");
+		pair<string, string> patient6("123456-1666", "Patient 6 info");
+
+		patientDatabase->insert(patient1);
+		patientDatabase->insert(patient2);
+		patientDatabase->insert(patient3);
+		patientDatabase->insert(patient4);
+		patientDatabase->insert(patient5);
+		patientDatabase->insert(patient6);
 	}
 
 
 	void handleEvent()
 	{
-		cout << "String recived from client: " << sockStream.recive() << endl;
+		string cprNr = sockStream.recive();
+
+		string patientInfo = (*patientDatabase)[cprNr];
+
+		cout << "String recived from client: " << cprNr << endl;
+
+		cout << "Sending response: " << patientInfo << endl;
+
+		sockStream.send(patientInfo);
+
 	}
 
 	void open()
@@ -28,5 +51,8 @@ public:
 	EventType getType() {
 		return EventType::PATIENTINFOEVENT;
 	}
+
+private:
+	shared_ptr<map<string, string>> patientDatabase;
 };
 
--- Opg4_5_Server/mainServer.cpp
+++ Opg4_5_Server/mainServer.cpp
@@ -12,21 +12,7 @@
 using namespace std;
 
 void main(){
-	auto patientDatabase = make_shared <map<string, string>>();
 
-	pair<string, string> patient1("123456-1111", "Patient 1 info");
-	pair<string, string> patient2("123456-1222", "Patient 2 info");
-	pair<string, string> patient3("123456-1333", "Patient 3 info");
-	pair<string, string> patient4("123456-1444", "Patient 4 info");
-	pair<string, string> patient5("123456-1555", "Patient 5 info");
-	pair<string, string> patient6("123456-1666", "Patient 6 info");
-	
-	patientDatabase->insert(patient1);
-	patientDatabase->insert(patient2);
-	patientDatabase->insert(patient3);
-	patientDatabase->insert(patient4);
-	patientDatabase->insert(patient5);
-	patientDatabase->insert(patient6);
 
 	//initiate server
 
@@ -47,8 +33,9 @@ void main(){
 
 	std::shared_ptr<Reactor> dispatcher = make_shared<Dispatcher>();
 
-	Acceptor<PatientServiceHandlerServer> accepter(dispatcher, EventType::PATIENTINFOEVENT);
+	auto accepter = make_shared < Acceptor<PatientServiceHandlerServer>>(dispatcher, EventType::PATIENTINFOEVENT);
 
+	dispatcher->registerHandler(accepter, EventType::ACCEPTOR);
 
 
 	//5: respond with PatientInfo
