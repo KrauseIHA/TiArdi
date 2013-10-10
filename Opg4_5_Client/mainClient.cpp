d5ed08947f63907dc0ecec5c03088bffef001d5e
Theis Nickelsen <theisnickelsen@gmail.com>
2013-10-10 17:37:28 +0200

    Pseudo code for client and server

--- Opg4_5_Client/mainClient.cpp
+++ Opg4_5_Client/mainClient.cpp
@@ -1,7 +1,22 @@
-
+#include "../Opg4AcceptorConnector/Connector.h"
 
 
 int main(){
+	
+	
+	
+	//1: Open socket connection to server
 
+	//2: Client inputs CPR number on the client PC
 
+	//3: Send GetPatientInfo(CPR number) request to server via TCP/IP
+
+	//wait for responce
+
+	//6: receives responce and displays received record
+
+	//must be possible to enter a new CPR number (repeat step 2-6).
+	//see server for 4 & 5.
+
+	//close application
 }
--- Opg4_5_Server/mainServer.cpp
+++ Opg4_5_Server/mainServer.cpp
@@ -1,6 +1,10 @@
 
 
 int main(){
+	//initiate server
 
+	//4: receives request and searches for patient info in a file
+
+	//5: respond with PatientInfo
 
 }
